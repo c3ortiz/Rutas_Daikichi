85e14d34be2393dab6d0dc199924c9d3d0d3051b
Cesar Ortiz <cesarortizbq@gmail.com>
2023-02-26 18:29:02 -0500

    Added @PathVariable annotation

--- src/main/java/com/codingdojo/daikichi/ControladorDaikichi.java
+++ src/main/java/com/codingdojo/daikichi/ControladorDaikichi.java
@@ -1,18 +1,19 @@
 package com.codingdojo.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/daikichi")
 public class ControladorDaikichi {
-    @RequestMapping("")
-    public String daikichi() {
-        return "¡Bienvenido!";
+    @RequestMapping("/travel/{city}")
+    public String viajeCiudad(@PathVariable("city") String city) {
+        return "¡Felicitaciones! ¡Pronto viajarás a " + city + "!";
     }
 
-    @RequestMapping("/today")
-    public String today() {
-        return "¡Hoy encontrará suerte en todos tus esfuerzos!";
+    @RequestMapping("/lotto/{number}")
+    public String lottoSuerte(@PathVariable("number") Integer number) {
+        return number % 2 == 0 ? "Harás un gran viaje en un futuro cercano, pero ten cuidado con las ofertas tentadoras" : "Has disfrutado de los frutos de tu trabajo, pero ahora es un buen momento para pasar tiempo con familiares y amigos";
     }
 }
